7290903ee41be01744e38f0fbc2cd350340e1e37
king <369558172@qq.com>
2018-04-17 15:49:52 +0800

    USE METHOD OBJECT REFACTOR PRACTICE3

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -16,24 +16,6 @@ public class Order {
     }
 
     public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
-
-        // Total up line items
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-
-        // Subtract discounts
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-
-        // calculate tax
-        BigDecimal tax = subTotal.multiply(this.tax);
-
-        // calculate GrandTotal
-        BigDecimal grandTotal = subTotal.add(tax);
-
-        return grandTotal;
+        return new PriceCaculator(this, orderLineItemList, discounts, tax).calculate();
     }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,41 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+    private final Order order;
+
+    private List<OrderLineItem> orderLineItemList;
+    private List<BigDecimal> discounts;
+    private BigDecimal tax;
+
+    public PriceCaculator(Order order, List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts, BigDecimal tax) {
+        this.order = order;
+        this.orderLineItemList = orderLineItemList;
+        this.discounts = discounts;
+        this.tax = tax;
+    }
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        // Subtract discounts
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+
+        // calculate tax
+        BigDecimal tax = subTotal.multiply(this.tax);
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
 }
